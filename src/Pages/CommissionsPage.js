ddde593bcadb7dd73f2f963890230a57e9219d4f
Bence Linder <bence.linder@gmail.com>
2021-02-17 21:00:16 -0500

    removed useless constructors

--- src/Pages/CommissionsPage.js
+++ src/Pages/CommissionsPage.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 class CommissionsPage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (<div>Home</div>);
