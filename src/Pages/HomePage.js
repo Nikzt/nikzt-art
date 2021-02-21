17e0e36697b2ea9ceaf0205369680cbaff5de010
Bence Linder <bence.linder@gmail.com>
2021-02-20 21:28:30 -0500

    updated home page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,9 +3,14 @@ import React from 'react';
 class HomePage extends React.Component {
 
     render() {
-        return (<div>
-            <img src={`${process.env.PUBLIC_URL}/assets/images/logo.png`}
-                alt="Nikzt's Logo"></img>
+        return (<div className="homepage">
+            <div className="bio">
+                <img src={`${process.env.PUBLIC_URL}/assets/images/logo.png`}
+                    alt="Nikzt's Logo" />
+                <h2>
+                    here I draw
+                </h2>
+            </div>
         </div>);
     }
 }
