5c0b1babafa44e8017ba4f66ea63095ffe0d50e3
Bence Linder <bence.linder@gmail.com>
2021-02-20 19:38:33 -0500

    added alt prop to image

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,7 +4,8 @@ class HomePage extends React.Component {
 
     render() {
         return (<div>Home
-            <img src={"http://localhost:3000/logo512.png"}></img>
+            <img src={"http://localhost:3000/logo512.png"}
+            alt="Nikzt's Logo"></img>
         </div>);
     }
 }
