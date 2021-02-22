41ac818b7a6cc0350030502679e010a5a166d4a5
Bence Linder <bence.linder@gmail.com>
2021-02-21 23:19:06 -0500

    added a gallery image

--- src/Pages/GalleryPage.js
+++ src/Pages/GalleryPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const GalleryPage = () => {
-    return (<div className="page">
+    return (<div className="gallery-page page">
         <h1>Gallery</h1>
+            <img src={`${process.env.PUBLIC_URL}/assets/images/witch-sketch1.png`}
+                alt="witch sketch" />
         </div>);
 }
 
