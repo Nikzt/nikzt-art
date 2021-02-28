ceb67d736b81cae1eefdc27f02fcf1fa504b0196
Nikzt <bence.linder@gmail.com>
2021-02-28 13:19:43 -0500

    secret link!

--- src/Pages/GalleryPage.js
+++ src/Pages/GalleryPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const GalleryPage = () => {
+    const openSecretLink = () => {
+        window.open("http://trailless.io/download/603bdc9698b39e1f4f13b060");
+    }
     return (<div className="gallery-page page">
         <h1>Gallery</h1>
             <img src={`${process.env.PUBLIC_URL}/assets/images/witch-sketch1.png`}
-                alt="witch sketch" />
+                alt="witch sketch" 
+                onClick={openSecretLink}/>
         </div>);
 }
 
